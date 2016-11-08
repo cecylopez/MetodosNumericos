d7dfb7a5a5b2dd7d8e43c3b8ae0b2d7e1446ceb5
robertux <blendboy@gmail.com>
2016-11-07 22:03:53 -0600

    Adición de UnitTest para validar el reconocimiento de símbolos en el Parser.

--- MetodosNumericos/src/com/metodosNumericos/test/TestParser.java
+++ MetodosNumericos/src/com/metodosNumericos/test/TestParser.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.metodosNumericos.util.Evaluador;
+
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
@@ -35,5 +37,30 @@ public class TestParser {
 		Expression eL = new ExpressionBuilder(ecuacion2).variables("x").build();
 		eL.setVariable("x", valorX);
 		assertEquals("El resultado de la evaluacion debe de ser 2.09183", 2.09183, eL.evaluate(),0.1);
+	}
+	
+	@Test
+	public void testSimbolos() {
+		//String simboloPI1 = "PI";
+		String simboloPI2 = "pi";
+		//String simboloPI3 = "Pi";
+		String simboloPI4 = "2pi";
+		String simboloPI5 = "-π";
+		String simboloPI6 = "2π";
+		
+		String simboloE1 = "e";
+		//String simboloE2 = "E";
+		String simboloE3 = "-e";
+		
+		//assertEquals("El valor debe reconocerse como 3.1416", 3.1416, Evaluador.evaluar(simboloPI1), 0.1);
+		assertEquals("El valor debe reconocerse como 3.1416", 3.1416, Evaluador.evaluar(simboloPI2), 0.1);
+		//assertEquals("El valor debe reconocerse como 6.2832", 6.2832, Evaluador.evaluar(simboloPI3), 0.1);
+		assertEquals("El valor debe reconocerse como 6.2832", 6.2832, Evaluador.evaluar(simboloPI4), 0.1);
+		assertEquals("El valor debe reconocerse como 3.1416", -3.1416, Evaluador.evaluar(simboloPI5), 0.1);
+		assertEquals("El valor debe reconocerse como 6.2832", 6.2832, Evaluador.evaluar(simboloPI6), 0.1);
+		
+		assertEquals("El valor debe reconocerse como 2.7182", 2.7182, Evaluador.evaluar(simboloE1), 0.1);
+		//assertEquals("El valor debe reconocerse como 2.7182", 2.7182, Evaluador.evaluar(simboloE2), 0.1);
+		assertEquals("El valor debe reconocerse como 2.7182", -2.7182, Evaluador.evaluar(simboloE3), 0.1);
 	}
 }
