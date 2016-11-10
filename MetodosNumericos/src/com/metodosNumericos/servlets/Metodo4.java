1fb18e484010151fe1fc2af42821980d432010a9
robertux <blendboy@gmail.com>
2016-11-09 22:07:27 -0600

    Correcciones finales para el funcionamiento del Método 4 para integrales dobles.

--- MetodosNumericos/src/com/metodosNumericos/servlets/Metodo4.java
+++ MetodosNumericos/src/com/metodosNumericos/servlets/Metodo4.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.metodosNumericos.util.Evaluador;
@@ -32,6 +31,11 @@ public class Metodo4 extends HttpServlet {
 		String limiteI2 = request.getParameter("limiteI2");
 		double limiteI3 = Evaluador.evaluar(request.getParameter("limiteI3"));
 		
+		System.out.println("n = " + n + " \t m = " + m + " \t o = " + o);
+		System.out.println("integralM = " + integralM);
+		System.out.println("limite S1 = " + limiteS1 + " \tS2 = " + limiteS2 + " \tS3 = " + limiteS3);
+		System.out.println("limite I1 = " + limiteI1 + " \tI2 = " + limiteI2 + " \tI3 = " + limiteI3);
+		
 		JsonObject obj = new JsonObject();
 		
 		//Paso 1
@@ -43,12 +47,19 @@ public class Metodo4 extends HttpServlet {
 		//Paso 2
 		for (int i=0; i<=n; i++) {
 			
+			System.out.println("======== PARA I = " + i);
 			//Paso 3
 			double x = limiteI1 + (i * h);
 			String HX = "(" + limiteS2 + " - " + limiteI2 + ") / " + m;
-			double K1 = Evaluador.evaluar(integralM.replaceAll("y", limiteI2), x) + Evaluador.evaluar(integralM.replaceAll("y", limiteS2), x);	//Términos extremos
+			double K1 = Evaluador.evaluar(integralM.replaceAll("y", "(" + limiteI2 + ")"), x) + Evaluador.evaluar(integralM.replaceAll("y", "(" + limiteS2 + ")"), x);	//Términos extremos
 			double K2 = 0;		//Términos pares
 			double K3 = 0;		//Términos impares
+			
+			System.out.println("\tx(" + i + ") = " + x);
+			System.out.println("\tHX(" + i + ") = " + HX);
+			System.out.println("\tK1(" + i + ") = " + K1 + " \tusando " + integralM.replaceAll("y", "(" + limiteI2 + ")") + " + " + integralM.replaceAll("y", "(" + limiteS2 + ")"));
+			System.out.println("\tK2(" + i + ") = " + K2);
+			System.out.println("\tK3(" + i + ") = " + K3);
 			
 			//Paso 4
 			for (int j=1; j<m; j++) {
@@ -57,6 +68,8 @@ public class Metodo4 extends HttpServlet {
 				double y = Evaluador.evaluar(limiteI2 + " +  (" + j + " * " + HX + ")", x);
 				double Q = Evaluador.evaluar(integralM, x, y);
 				
+				System.out.println("\t\ty(" + j + ") = " + y);
+				System.out.println("\t\tQ(" + j + ") = " + Q);
 				//Paso 6
 				if (j % 2 == 0) {
 					K2 += Q;
@@ -67,6 +80,7 @@ public class Metodo4 extends HttpServlet {
 			
 			//Paso 7
 			double L = (K1 + (2.0 * K2) + (4.0 * K3)) * (Evaluador.evaluar(HX, x) / 3.0);
+			System.out.println("\tL(" + i + ") = " + L);
 			
 			//Paso 8
 			if (i == 0 || i == n) {
@@ -76,10 +90,16 @@ public class Metodo4 extends HttpServlet {
 			} else {
 				j3 += L;
 			}
+			
+			System.out.println("\tj1(" + i + ") = " + j1);
+			System.out.println("\tj2(" + i + ") = " + j2);
+			System.out.println("\tj3(" + i + ") = " + j3);
 		}
 		
 		//Paso 9
 		double J = (j1 + (2 * j2) + (4 * j3)) * (h/3);
+		
+		System.out.println("\n\nJ = " + J);
 		
 		obj.add("resultado", new JsonPrimitive(J));
 		
